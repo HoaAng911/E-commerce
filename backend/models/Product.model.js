1b4c320db5298dc137f6f137c9abf392f90790bc
HoaAng911 <mayanh279@gmail.com>
2025-09-18 13:33:13 +0700

    Tao models Product

---
+++ backend/models/Product.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+    min: 0
+  },
+  sizes: {
+    type: [String],
+    default: []
+  },
+  colors: {
+    type: [String],
+    default: []
+  },
+  stock: {
+    type: Number,
+    default: 0
+  },
+  images: {
+    type: [String],
+    default: []
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+}
+
+)
+
+export default mongoose.model('Product', ProductSchema)
